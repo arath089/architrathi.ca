731c2f7e5fdc637dd8ce9ed09bfe2bf321aaa924
Archit Rathi <architrathi@Archits-MacBook-Pro-2.local>
2025-07-02 01:29:31 -0700

    feat: Revamp initial commit: Setup NextJS app + Tailwind

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 flex items-center justify-center text-white text-4xl font-bold">
+      Tailwind is working! 🚀
+    </main>
+  );
+}
